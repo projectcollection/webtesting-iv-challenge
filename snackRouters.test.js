0d944dddff07ae3bd83d85158f8c6183c7ccc433
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-11 18:14:46 -0400

    Wrote tests for snackRouter

--- snackRouters.test.js
+++ snackRouters.test.js
@@ -1,3 +1,39 @@
 const request = require('supertest');
 
 const snackRouters = require('./snackRouters');
+
+describe('snackRouters', () => {
+    describe('POST /snacks', () => {
+        test('should respond with 201', () => {
+            return request(snackRouters)
+                .post('/snacks')
+                .send({
+                    name: "Fruit"
+                })
+                .expect(201)
+        });
+
+        test('should respond with json', () => {
+            return request(snackRouters)
+                .post('/snacks')
+                .send({
+                    name: "Fruit"
+                })
+                .expect('Content-Type', /json/)
+        })
+    });
+
+    describe('DELETE /snack', () => {
+        test('should respond with 200', () => {
+            return request(snackRouters)
+                .del('/snacks/' + 1)
+                .expect(200)
+        });
+
+        test('should respond with json', () => {
+            return request(snackRouters)
+                .del('/snacks')
+                .expect('Content-Type', /json/)
+        })
+    })
+})
